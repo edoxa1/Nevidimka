a42c92574afbbb36bf0f7b2943bbd66313002890
eraidai58@gmail.com <eraidai58@gmail.com>
2020-01-23 22:23:00 +0600

    Добавьте файлы проекта.

---
+++ Nevidimka/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nevidimka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            label1.Text = "Невидим";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            label1.Text = "Видим";
+        }
+    }
+}
